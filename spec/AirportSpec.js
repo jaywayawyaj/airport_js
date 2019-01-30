b8e3e79e09beb36f6c5680e554a8337bfedc1876
Jethro Whaley <jethro.whaley@gmail.com>
2019-01-30 20:54:35 +0000

    rejigged test suite for stormy/non stormy conditions

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -6,23 +6,35 @@ describe('Airport', function() {
     plane = new Plane();
   });
 
-  describe('#land', function() {
+  describe('non stormy', function() {
+    beforeEach(function() {
+      spyOn(airport.weather, 'isStormy').and.returnValue(false);
+    })
+
     it('lands a plane', function() {
       airport.land(plane);
       expect(airport.hangar.length).toEqual(1);
     });
 
-    it('cannot land if it is stormy', function() {
-      spyOn(airport.weather, 'isStormy').and.returnValue(true);
-      expect(function(){ airport.land(plane) }).toThrow('It is too stormy to land!');
-    })
-  });
-
-  describe('#takeoff', function() {
     it('a plane can takeoff from the hangar', function() {
       airport.land(plane);
       airport.takeoff(plane);
       expect(airport.hangar.length).toEqual(0);
     });
+
+
+  });
+
+  describe('stormy weather', function() {
+
+    describe('non stormy', function() {
+      beforeEach(function() {
+        spyOn(airport.weather, 'isStormy').and.returnValue(true);
+      })
+
+      it('cannot land if it is stormy', function() {
+        expect(function(){ airport.land(plane) }).toThrow('It is too stormy to land!');
+      })
+    })
   });
 });
